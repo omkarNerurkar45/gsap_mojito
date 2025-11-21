d3aef7bd5b550596a20bea0edde967d483c7b12a
Omkar Nerurkar <omkarnerurkar7@gmail.com>
2025-11-21 04:42:22 -0600

    Created a menu section with smooth slider

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import Art from './components/Art';
+import Menu from './components/Menu';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -15,6 +16,7 @@ const App = () => {
       <Hero />
       <Cocktails />
       <Art />
+      <Menu />
     </main>
   )
 }
